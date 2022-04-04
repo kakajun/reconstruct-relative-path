d2958fe23f4c888d1189e9648159dc017231abfa
马军 <majun@xiwicloud.com>
2022-04-04 10:42:47 +0800

    fix: 1

--- unuse/main.js
+++ unuse/main.js
@@ -1,1 +1,10 @@
-import { createApp } from 'vue'import App from './App.vue'// import '../lib/style.css'import SketchRule from './unuse/component/user-ruler.vue'const app = createApp(App)app.use(SketchRule)// const MyComponent = app.component('SketchRule')// console.log(MyComponent 'MyComponentMyComponent')app.mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+// import '../lib/style.css'
+import SketchRule from '@/unuse/component/user-ruler.vue'
+const app = createApp(App)
+app.use(SketchRule)
+
+// const MyComponent = app.component('SketchRule')
+// console.log(MyComponent, 'MyComponentMyComponent')
+app.mount('#app')
