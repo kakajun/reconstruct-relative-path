7e69ce5c10d78c40ff2baf289e40addeb0348353
马军 <253495832@qq.com>
2022-04-05 12:44:06 +0800

    fix: 写文件功能完成

--- src/mark-write-file.ts
+++ src/mark-write-file.ts
@@ -8,8 +8,9 @@
  */
 import createDebugger from 'debug'
 import { findNodes } from './mark-file'
-import fs from 'fs'
 import { ItemType } from './get-file'
+import fs from 'fs-extra';
+
 const debug = createDebugger('mark-write-file')
 debug.enabled = true
 
@@ -21,7 +22,7 @@ debug.enabled = true
  * @param {string} path  绝对路径
  * @param {string} rootPath   确定哪一级开始创建文件夹
  */
-export function markWriteFile(nodes: ItemType[], name: string, path: string, rootPath: string) {
+export  function markWriteFile(nodes: ItemType[], name: string, path: string, rootPath: string) {
   // debug('入参: ', name, path)
   // 通过文件地址, 找到nodes的依赖地址, 把依赖文件也打标记
   const node = findNodes(nodes, path)
@@ -30,7 +31,7 @@ export function markWriteFile(nodes: ItemType[], name: string, path: string, roo
     // 得到标记
     const belongTo = node.belongTo
     if (belongTo.length > 0) {
-      setDispFile(path, name, rootPath)
+      setDispFileNew(path, name, rootPath)
     }
     // 找到有子文件了,循环它
     for (let index = 0; index < node.imports.length; index++) {
@@ -48,9 +49,35 @@ export function markWriteFile(nodes: ItemType[], name: string, path: string, roo
 }
 
 /**
+ * @desc: 功能就是找到文件然后copy文件,
+ *  特别说明: 之前想着自己造轮子,递归写了好几个, 自己都调晕了, 在下面注释的好大段代码写和调试花了半天, 结果还有写文件为空的bug解决不了, 不知道原因是什么,
+ *  后面网上找到插件fs-extra, 结果就下面一段代码轻而易举就实现了功能, 还只花了几分钟,靠.......
+  * *******************总结教训, 不要轻易自己造轮子, 否则轮子砸到脚痛还不讲, 还得进医院***********************
+ * @author: majun
+ * @param {string} path
+ * @param {string} name
+ * @param {string} rootPath
+ */
+function setDispFileNew(path: string, name: string, rootPath: string) {
+  debug('copyFile入参: ', name, path, rootPath)
+   const relative = path.replace(rootPath, '')
+   const originPath = path
+   const writeFileName = rootPath + '\\' + name + relative
+   debug('originPath: ', originPath)
+   debug('写入文件: ', writeFileName)
+   try {
+     fs.copy(originPath, writeFileName)
+     console.log('success!')
+   } catch (err) {
+     console.error(err)
+   }
+}
+
+/**
  * @desc: 这里递归创建文件夹和写文件-文件内递归, 文件内处理, 不需要node
  *       怎么知道原始路径? 就是把name一级干掉即可,干掉name级别就和外面的一模一样
  * 这里就干两件事,1 创建文件夹, 2 创建文件
+
  *
  * @author: majun
  * @param {string} path    path 初始化时是原绝对路径, 后面就是已创建文件夹后的路径
@@ -58,40 +85,40 @@ export function markWriteFile(nodes: ItemType[], name: string, path: string, roo
  * @param {string} rootPath  一直是跟路径
  * @param {string} restName  递归创建文件夹的剩余name
  */
-function setDispFile(path: string, name: string, rootPath: string, restName?: string) {
-  debug('setDispFile入参: ', path, name)
-  let seconFlag = path.indexOf(name) > -1
-  // 1. 依次找到最外层文件夹
-  const relative =restName?restName: path.replace(rootPath + '\\', '')
-  const foldNameArrs = relative.split('\\')
-  debug('relative: ', relative)
-  debug('foldNameArrs: ', foldNameArrs)
-  //如果是文件, copy文件
-  if (foldNameArrs[0].indexOf('.') > -1) {
-    if (seconFlag) {
-      copyFile(path, name, foldNameArrs[0]);
-    } else {
-      const newPath = rootPath + '\\' + name + '\\' + relative
-      copyFile(newPath, name, foldNameArrs[0])
-    }
-  } else {
-    // 下面处理文件夹递归关系
-    if (seconFlag) {
-      //还不是文件, 文件夹创建
-      setFolder(path, foldNameArrs[0])
-      path = path + '\\' + foldNameArrs[0]
-    } else {
-      //还不是文件, 文件夹创建--第一次
-      setFolder(rootPath + '\\' + name, foldNameArrs[0])
-      path = rootPath + '\\' + name + '\\' + foldNameArrs[0]
-    }
-    // 递归之前要去掉已建文件夹
-     foldNameArrs.splice(0, 1)
-    const newName = foldNameArrs.join('\\')
-    debug('递归参数: ', path, newName)
-    setDispFile(path, name, rootPath, newName)
-  }
-}
+// function setDispFile(path: string, name: string, rootPath: string, restName?: string) {
+//   debug('setDispFile入参: ', path, name)
+//   let seconFlag = path.indexOf(name) > -1
+//   // 1. 依次找到最外层文件夹
+//   const relative =restName?restName: path.replace(rootPath + '\\', '')
+//   const foldNameArrs = relative.split('\\')
+//   debug('relative: ', relative)
+//   debug('foldNameArrs: ', foldNameArrs)
+//   //如果是文件, copy文件
+//   if (foldNameArrs[0].indexOf('.') > -1) {
+//     if (seconFlag) {
+//       copyFile(path, name, foldNameArrs[0]);
+//     } else {
+//       const newPath = rootPath + '\\' + name + '\\' + relative
+//       copyFile(newPath, name, foldNameArrs[0])
+//     }
+//   } else {
+//     // 下面处理文件夹递归关系
+//     if (seconFlag) {
+//       //还不是文件, 文件夹创建
+//       setFolder(path, foldNameArrs[0])
+//       path = path + '\\' + foldNameArrs[0]
+//     } else {
+//       //还不是文件, 文件夹创建--第一次
+//       setFolder(rootPath + '\\' + name, foldNameArrs[0])
+//       path = rootPath + '\\' + name + '\\' + foldNameArrs[0]
+//     }
+//     // 递归之前要去掉已建文件夹
+//      foldNameArrs.splice(0, 1)
+//     const newName = foldNameArrs.join('\\')
+//     debug('递归参数: ', path, newName)
+//     setDispFile(path, name, rootPath, newName)
+//   }
+// }
 
 /**
  * @desc: 读取原文件,写入到新地址
@@ -100,26 +127,42 @@ function setDispFile(path: string, name: string, rootPath: string, restName?: st
  * @param {string} name
  * @param {string} filName 要copy的文件名
  */
-export function copyFile(path: string, name: string, filName: string) {
-  debug('copyFile入参: ', name, path, filName)
-  const origin = path.replace(name + '\\', '')
-  const flag = path.indexOf('.')>-1
-  const originPath = flag ? origin : origin + '\\' + filName
-  const writeFileName = flag ? path : path + '\\' + filName
-  debug('originPath: ', originPath)
-   debug('写入文件: ', writeFileName)
-  const str = fs.readFileSync(originPath, 'utf-8')
-  console.log(str, "666")
-      const str2 = fs.readFileSync(
-        'D:\\worker\\reconstruct-relative-path\\unuse\\components\\test\\deep\\user-ruler.vue',
-        'utf-8'
-      )
-      console.log(str2.toString(), '21321')
-  // 异步写入数据到文件
-  fs.writeFile(writeFileName, str, { encoding: 'utf8' }, () => {
-    console.log('newfile write successful', writeFileName)
-  })
-}
+// export function copyFile(path: string, name: string, filName: string) {
+//   debug('copyFile入参: ', name, path, filName)
+//   const origin = path.replace(name + '\\', '')
+//   const flag = path.indexOf('.')>-1
+//   const originPath = flag ? origin : origin + '\\' + filName
+//   const writeFileName = flag ? path : path + '\\' + filName
+//   debug('originPath: ', originPath)
+//    debug('写入文件: ', writeFileName)
+//   const str = fs.readFileSync(originPath, 'utf-8')
+//   console.log(str, "666")
+//       const str2 = fs.readFileSync(
+//         'D:\\worker\\reconstruct-relative-path\\unuse\\components\\test\\deep\\user-ruler.vue',
+//         'utf-8'
+//       )
+//       console.log(str2.toString(), '21321')
+//   // 异步写入数据到文件
+//   fs.writeFile(writeFileName, str, { encoding: 'utf8' }, () => {
+//     console.log('newfile write successful', writeFileName)
+//   })
+// }
+
+// function copyFile(path: string, name: string, filName: string) {
+//   debug('copyFile入参: ', name, path, filName)
+//   const origin = path.replace(name + '\\', '')
+//   const flag = path.indexOf('.') > -1
+//   const originPath = flag ? origin : origin + '\\' + filName
+//   const writeFileName = flag ? path : path + '\\' + filName
+//   debug('originPath: ', originPath)
+//   debug('写入文件: ', writeFileName)
+//   try {
+//     fs.copy(originPath, writeFileName)
+//     console.log('success!')
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
 
 /**
  * @desc: 给一个路径和包名,然后就创建文件夹,如果存在那就啥也不管
